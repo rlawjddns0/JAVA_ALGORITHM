1f3eb97b48bf0e58520d027faca2754ecfbeced6
KIMJEONGWOON <rlawjddns0@naver.com>
2021-01-09 22:45:22 +0900

    백준

--- ALGO_JAVA/src/BACK/main.java
+++ ALGO_JAVA/src/BACK/main.java
@@ -49,11 +49,11 @@ public class main {
 //
 //		수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
 
-		Scanner scanner=new Scanner(System.in);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
 		Set<Integer> number=new HashSet<Integer>();
 		for(int i=0; i<10; i++) {
-			int num=scanner.nextInt();
-			number.add(num%42);
+			number.add(Integer.parseInt(br.readLine())%42);
 			
 		}
 		System.out.println(number.size());
