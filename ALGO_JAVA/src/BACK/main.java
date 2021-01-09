db17d66a76b4a642fa6c6a0c4a66ebf6980ed889
KIMJEONGWOON <rlawjddns0@naver.com>
2021-01-09 20:12:52 +0900

    백준

--- ALGO_JAVA/src/BACK/main.java
+++ ALGO_JAVA/src/BACK/main.java
@@ -3,7 +3,9 @@ package BACK;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.StringTokenizer;
 //BACK_10950
 public class main {
@@ -26,22 +28,35 @@ public class main {
 //		}
 		//보통의 scaaner 방식
 		
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int Test_Case=Integer.parseInt(br.readLine());
+//		
+//		StringTokenizer st;
+//		StringBuilder sb=new StringBuilder();
+//		
+//		for(int i=0; i<Test_Case; i++) {
+//			st=new StringTokenizer(br.readLine()," ");
+//			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+//			sb.append('\n');
+//		}
+//		
+//		
+//		System.out.println(sb);
 		
-		int Test_Case=Integer.parseInt(br.readLine());
 		
-		StringTokenizer st;
-		StringBuilder sb=new StringBuilder();
-		
-		for(int i=0; i<Test_Case; i++) {
-			st=new StringTokenizer(br.readLine()," ");
-			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
-			sb.append('\n');
+//		두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
+//
+//		수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
+
+		Scanner scanner=new Scanner(System.in);
+		Set<Integer> number=new HashSet<Integer>();
+		for(int i=0; i<10; i++) {
+			int num=scanner.nextInt();
+			number.add(num%42);
+			
 		}
-		
-		
-		System.out.println(sb);
-		
+		System.out.println(number.size());
 
 	}
 
