684250c6612f6c3025fe6cbbc55cf42f9f6d18c0
rlawjddns0 <rlawjddns0@naver.com>
2021-01-25 01:49:35 +0900

    day 20210125

---
+++ src/BACK/day_20210123/BACK_16922.java
@@ -0,0 +1,21 @@
+package BACK.day_20210123;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BACK_16922 {
+	private final static int[] number= {1,5,10,50};
+	private static int answer;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		//solution(n);
+		
+		
+	}
+	
+
+}
---
+++ src/SWE/day_20210125/SWE_1974.java
@@ -0,0 +1,62 @@
+package SWE.day_20210125;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class SWE_1974 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int testCase=Integer.parseInt(br.readLine());
+		
+		
+		for(int t=0; t<testCase; t++) {
+			
+			int[][] arr=new int[9][9];
+			
+			for(int i=0; i<9; i++) {
+				st=new StringTokenizer(br.readLine()," ");
+				for(int j=0; j<9; j++) {
+					arr[i][j]=Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			boolean flag=true;
+			
+			
+			for(int i=0; i<9; i++) {
+				Arrays.sort(arr[i]);
+				int sum=0;
+				for(int j=0; j<9; j++) {
+					sum+=arr[i][j];
+					
+				}
+				//System.out.println(sum);
+				if(sum!=45) {
+					flag=false;
+					break;
+				}
+				
+				
+				
+			}
+			System.out.print("#"+(t+1)+" ");
+			if(flag) {
+				System.out.print("1");
+			}
+			else
+				System.out.print("0");
+			
+			System.out.println();
+			
+			
+		}
+	}
+
+}
