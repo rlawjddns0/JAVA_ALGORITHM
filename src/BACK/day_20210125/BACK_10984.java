a0fa9d3953eee7344aea536381c889e6287f591d
rlawjddns0 <rlawjddns0@naver.com>
2021-01-25 23:41:09 +0900

    20210125

--- src/BACK/day_20210125/BACK_10984.java
+++ src/BACK/day_20210125/BACK_10984.java
@@ -1,11 +1,29 @@
 package BACK.day_20210125;
 
+import java.util.Scanner;
+
 public class BACK_10984 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int sum1=0;
+		int sum2=0;
+		for(int i=0; i<4; i++)
+		{
+			sum1+=sc.nextInt();
+		}
 		
-
+		for(int i=0; i<4; i++)
+			sum2+=sc.nextInt();
+		
+		
+		if(sum1<sum2)
+			System.out.println(sum2);
+		else if(sum1> sum2)
+			System.out.println(sum1);
+		else
+			System.out.println(sum1);
 	}
 
 }
