beaecb52edd4edcb42f91fecd4e218d66c04e774
rlawjddns0 <rlawjddns0@naver.com>
2021-02-02 23:00:03 +0900

    20210202

--- src/BACK/day_20210201/BACK_16926.java
+++ src/BACK/day_20210201/BACK_16926.java
@@ -6,77 +6,75 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BACK_16926 {
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] dy = { 0, 1, 0, -1 };
-	static int N, M, rotation, num;
-	static int[][] arr;
-	static StringBuilder sb = new StringBuilder();
-
+	private static int n, m, r;
+	private static int[][] map;
+	
+	private static int[] dx = {0, 1, 0, -1};//우상좌하
+	private static int[] dy = {1, 0, -1, 0};
+	
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		rotation = Integer.parseInt(st.nextToken());
-		num = Math.min(N, M) / 2;
-
-		arr = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(reader.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		
+		map = new int[n+1][m+1];
+		
+		for (int i=1; i<=n; i++) {
+			st = new StringTokenizer(reader.readLine());
+			for (int j=1; j<=m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		for (int i = 0; i < rotation; i++) {
-			rotationMap();
+		
+		//총 돌려야 하는 네모 개수
+		int num = Math.min(n, m)/2;
+		
+		for (int i=0; i<r; i++) {
+			circulate(num);
 		}
-
-		resultPrint();
-
-	}
-
-	private static void rotationMap() {
-
-		for (int i = 0; i < num; i++) {
-			// 네모들의 왼쪽 맨위의 좌표는 1,1 || 2,2 || 3,3 || ...
-			int x = i;
-			int y = i;
-			int temp = arr[x][y]; // 첫번째 값을 담아놓고 연산 후에 넣어준다.
-
-			int index = 0;
-
-			while (index < 4) {
-
-				int nx = x + dx[index];
-				int ny = y + dy[index];
-
-				if (nx < i || ny < i || nx > M - i + 1 || ny > N - i + 1) {
-					index++;
-				} else {
-					arr[x][y] = arr[nx][ny];
-
-					x = nx;
-					y = ny;
-				}
-
-			} // while
-			arr[i + 1][i] = temp;
-		} // for
-
-	}// circulate()
-
-	private static void resultPrint() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				System.out.print(arr[i][j] + " ");
+		
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<=m; j++) {
+				System.out.print(map[i][j] + " ");
 			}
 			System.out.println();
 		}
+		
+	}//main
 
-	}
+	private static void circulate(int num) {
+		
+		for (int i=1; i<=num; i++) {
+			//네모들의 왼쪽 맨위의 좌표는 1,1 || 2,2 || 3,3 || ...
+			int x = i;
+			int y = i;
+			int temp = map[x][y]; //첫번째 값을 담아놓고 연산 후에 넣어준다.
+			
+			int index = 0;
+			
+			while (index < 4) {
+				
+				int nx = x + dx[index];
+				int ny = y + dy[index];
+				
+				if (nx < i || ny < i || nx > n-i+1 || ny > m-i+1) {
+					index++;
+				} else {
+					map[x][y] = map[nx][ny];
+					
+					x = nx;
+					y = ny;
+				}
+				
+			}//while
+			map[i+1][i] = temp;
+		}//for
+		
+	}//circulate()
 
 }
