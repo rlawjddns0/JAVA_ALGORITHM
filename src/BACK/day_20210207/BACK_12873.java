613b77fd2b963806708e34c09f5e7b811ea967f0
rlawjddns0 <rlawjddns0@naver.com>
2021-02-08 02:24:16 +0900

    algorithm

--- src/BACK/day_20210207/BACK_12873.java
+++ src/BACK/day_20210207/BACK_12873.java
@@ -7,38 +7,37 @@ import java.util.Scanner;
 
 public class BACK_12873 {
 	static int[] arr;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-				Scanner sc=new Scanner(System.in);
-				Deque<Integer> q=new LinkedList<Integer>();
-				int N=sc.nextInt();
-				
-				for(int i=1; i<=N; i++) {
-					q.offer(i);
-				}
-				int t=2;
-				q.poll();
-				while(q.size()!=1) {
-					int n=(int) Math.pow(t, 3);
-					
-					for(int i=0; i<n%q.size(); i++) {
-						q.offer(q.poll());
-					}
-					q.pollLast();
-					t++;
-				}
-				
-				System.out.println(q.poll());
+		Scanner sc = new Scanner(System.in);
+		Deque<Integer> q = new LinkedList<Integer>();
+		int N = sc.nextInt();
 
-	}
-	
-	public static void move() {
-		int temp = arr[7];
-		for (int i = 7; i >= 1; i--) {
-			arr[i] = arr[i - 1];
+		for (int i = 1; i <= N; i++) {
+			q.offer(i);
 		}
-		arr[0] = temp;
+		if (N == 1) {
+			System.out.println("1");
+		} else {
+			long t = 2;
+			q.poll();
+			while (q.size() != 1) {
+				long n = (long) Math.pow(t, 3);
+				n %= q.size();
+				if (n == 0) {
+					n = q.size();
+				}
+				for (int i = 0; i < n - 1; i++) {
+					q.offer(q.poll());
+				}
+				q.poll();
+
+				t++;
+			}
+		}
+		System.out.println(q.poll());
 	}
 
 }
---
+++ src/BACK/day_20210207/BACK_9012.java
@@ -0,0 +1,53 @@
+package BACK.day_20210207;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BACK_9012 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int testCase=Integer.parseInt(br.readLine());
+		
+		for(int t=1; t<=testCase; t++) {
+			String str=br.readLine();
+			char[] ch=str.toCharArray();
+			Stack<Character> stack=new Stack<>();
+			int i=0;
+			for(i=0; i<ch.length; i++) {
+				if(ch[i]=='(') {
+					stack.push(ch[i]);
+				}
+				else if(ch[i]==')'){
+					if(stack.isEmpty()) {
+						//System.out.println("NO");
+						break;
+					}
+					if(stack.peek()==')'  ) {
+						//System.out.println("NO");
+						break;
+					}else{
+						stack.pop();
+					}
+				}
+			}
+			
+			if(i==ch.length && stack.isEmpty()) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+			
+			
+		}
+		
+	}
+
+}
