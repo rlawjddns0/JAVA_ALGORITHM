f17c6ea9d16cfd78da0198448fe7da244e24cd15
rlawjddns0 <rlawjddns0@naver.com>
2021-02-23 13:18:38 +0900

    카카오 기출 풀이

--- src/Programers/day_20210220/프렌즈4블록.java
+++ src/Programers/day_20210220/프렌즈4블록.java
@@ -5,18 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class 프렌즈4블록 {
-	static int m, n;
-	static char[][] map = new char[m][n];
-	static char[][] tmap = new char[m][n];// 복제
-	static boolean[][] visit = new boolean[m][n];
-	static int[] dx = { 1, 0, 1 };
-	static int[] dy = { 0, 1, 1 };
-	static HashSet<loc> loc = new HashSet<loc>();
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
@@ -30,11 +20,13 @@ public class 프렌즈4블록 {
 		}
 
 		int ans = solution(m, n, str);
-		for (int i = 0; i < m; i++) {
-			System.out.println(Arrays.toString(map[i]));
-		}
+	
 		System.out.println(ans);
 	}
+
+	static int m, n;
+	static char[][] map = new char[m][n];
+	static char[][] tmap = new char[m][n];// 복제
 
 	public static int solution(int m, int n, String[] board) {
 
@@ -44,85 +36,67 @@ public class 프렌즈4블록 {
 			map[i] = board[i].toCharArray();
 			tmap[i] = board[i].toCharArray();
 		}
-		for (int i = 0; i < m; i++) {
-			System.out.println(Arrays.toString(map[i]));
+		
+		//한번 돌린다.
+		int temp=0;
+		for (int i = 0; i < m - 1; i++) {
+			for (int j = 0; j < n - 1; j++) {
+				solve(i, j);
+			}
 		}
-		System.out.println();
-		for(int p=0; p<m; p++) {
-			flag = false;
+		move();
+		temp=count;
+		for (int i = 0; i < m; i++) {
+			tmap[i] = map[i].clone();
+		}
+		
+		
+		//반복
+		while(true) {
+			count=0;
 			for (int i = 0; i < m - 1; i++) {
 				for (int j = 0; j < n - 1; j++) {
 					solve(i, j);
 				}
 			}
-			
 			move();
-			
-		}
-		for(int i=0; i<m; i++) {
-			for(int j=0; j<n; j++) {
-				if(map[i][j]=='.')
-					count++;
+			if(count==temp)break;//직전 깨진 블록의 수와 지금 깨진 블록의 수가 같으면 더이상 돌 필요 없다
+			else temp=count;//다르면 블록 갱신
+			for (int i = 0; i < m; i++) {
+				tmap[i] = map[i].clone();//임시 맵에 복제
 			}
 		}
-	
-//		solve(1,0);
-//		//move();
-//		for (int i = 0; i < m; i++) {
-//			System.out.println(Arrays.toString(map[i]));
-//		}
-//		System.out.println();
-//		solve(2,1);
-//		move();
-	return count;}
+		
+		
+		
+
+		return count;
+	}
 
 	static int count;
-	static boolean flag;
 
 	public static void solve(int y, int x) {
 
 		if (map[y][x] == '.') {
 			char tmp = tmap[y][x];
-//			if (tmp == map[y][x + 1] && tmp == map[y + 1][x + 1]) {
-//				flag = true;
-//				map[y][x + 1] = '.';
-//				map[y + 1][x + 1] = '.';
-//				count += 2;
-//			} else if (tmp == map[y][x + 1] && tmp == map[y + 1][x] && tmp == map[y + 1][x + 1]) {
-//				flag = true;
-//				map[y + 1][x] = '.';
-//				map[y][x + 1] = '.';
-//				map[y + 1][x + 1] = '.';
-//				count += 3;
-//			} else if (tmp == map[y + 1][x] && tmp == map[y + 1][x + 1]) {
-//				flag = true;
-//				map[y + 1][x] = '.';
-//				map[y + 1][x + 1] = '.';
-//				count += 2;
-//			}
-			if (tmp == tmap[y][x + 1] && tmp == tmap[y + 1][x] && tmp == tmap[y + 1][x + 1]) {
-				flag = true;
+			if (tmp == tmap[y][x + 1] && tmp == tmap[y + 1][x] && tmp == tmap[y + 1][x + 1]) {//.이 나와 있는 위치에서 검사
 				map[y + 1][x] = '.';
 				map[y][x + 1] = '.';
 				map[y + 1][x + 1] = '.';
-				
-				// count += 3;
 			}
 
 		}
-		if (map[y][x] == map[y][x + 1] && map[y][x] == map[y + 1][x] && map[y][x] == map[y + 1][x + 1]) {
-			flag = true;
+		if (map[y][x] == tmap[y][x + 1] && map[y][x] == tmap[y + 1][x] && map[y][x] == tmap[y + 1][x + 1]) {//2*2 블록이 같으면
 			map[y][x] = '.';
 			map[y + 1][x] = '.';
 			map[y][x + 1] = '.';
 			map[y + 1][x + 1] = '.';
-			
-			// count += 4;
 
 		}
+		
 	}
 
-	private static void move() {
+	private static void move() {//중력
 		for (int i = 0; i < n; i++) {
 			for (int j = m - 1; j >= 0; j--) {
 				int k = j;
@@ -135,32 +109,11 @@ public class 프렌즈4블록 {
 			}
 
 		}
+		for (int i = 0; i < m; i++) {//중력 후 블록 사라진 갯수 확인
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] == '.')
+					count++;
+			}
+		}
 	}
-}
-
-class loc {
-	int x;
-	int y;
-
-	loc(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return Objects.hash(x, y);
-
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof loc))
-			return false;
-		loc p = (loc) obj;
-		return this.x == (p.x) && this.y == p.y;
-
-	};
-
 }
