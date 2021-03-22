138b29d037fe0bc252c589bc752c5b3f9ec0268a
rlawjddns0 <rlawjddns0@naver.com>
2021-03-23 03:52:58 +0900

    알고리즘 풀이

--- src/BAEK/month_202103/day_20210321/BAEK_14499.java
+++ src/BAEK/month_202103/day_20210321/BAEK_14499.java
@@ -1,5 +1,7 @@
 package BAEK.month_202103.day_20210321;
 
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BAEK_14499 {
@@ -9,22 +11,17 @@ public class BAEK_14499 {
 	static int x,y;
 	static int K;
 	static int[] dx= {1,-1,0,0};//동쪽은 1, 서쪽은 2, 북쪽은 3, 남쪽은 4
-	static int[] dy= {0,0,1,-1};
-	static int[][] dice= {	{0,2,0},
-							{4,1,3},
-							{0,5,0},
-							{0,6,0}
-						};
-	static int dicex=1;
-	static int dicey=1;
+	static int[] dy= {0,0,-1,1};
+	static int[] dice = new int[7];
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		
 		N=sc.nextInt();
 		M=sc.nextInt();
-		x=sc.nextInt();
 		y=sc.nextInt();
+		x=sc.nextInt();
 		K=sc.nextInt();
 		order=new int[K];
 		map=new int[N][M];
@@ -38,31 +35,27 @@ public class BAEK_14499 {
 		}
 		for(int i=0; i<K; i++) {
 			int o=order[i]-1;
-			x+=dy[o];
-			y+=dx[o];
+			int nx=x+dx[o];
+			int ny=y+dy[o];
 			
-			if(x<0 || x>=N || y<0 || y>=M) {
-				x-=dy[o];
-				y-=dx[o];
-				
+			if(ny<0 || ny>=N || nx<0 || nx>=M) {
 				continue;
 			}
-			dicex+=dx[o];
-			dicey+=dy[o];
+			
+			
 			diceCheck(o);
 			
+			if(map[ny][nx]==0) {
+				map[ny][nx]=dice[6];
+			}
+			else {
+				dice[6]=map[ny][nx];
+				map[ny][nx]=0;
+			}
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			x=nx;
+			y=ny;
+			System.out.println(dice[1]);
 		}
 		
 		
@@ -70,19 +63,33 @@ public class BAEK_14499 {
 		
 		
 	}
-	private static void diceCheck(int o) {
-		if(o==0) {//동
-			if(dicex==2 && dicey==0) {dicex=1; dicey=0;}
-			if(dicex==2 && dicey==1) {dicex=0; dicey=1;}
-			if(dicex==2 && dicey==2) {dicex=1; dicey=1;}
-			if(dicex==2 && dicey==3) {dicex=0; dicey=1;}
-		}else if(o==1) {//서
-			
-		}else if(o==2) {//남
-			
-		}else {//북
-			
-		}
+	private static void diceCheck(int d) {
+		
+		int[] temp = dice.clone();
+	    // 동쪽은 0, 서쪽은 1, 북쪽은 2, 남쪽은 3
+	    if (d == 0) {
+	        dice[1] = temp[4];
+	        dice[3] = temp[1];
+	        dice[4] = temp[6];
+	        dice[6] = temp[3];
+	    } else if (d == 1) {
+	        dice[1] = temp[3];
+	        dice[3] = temp[6];
+	        dice[4] = temp[1];
+	        dice[6] = temp[4];
+	    } else if (d == 2) {
+	        dice[1] = temp[5];
+	        dice[2] = temp[1];
+	        dice[5] = temp[6];
+	        dice[6] = temp[2];
+	    } else {
+	        dice[1] = temp[2];
+	        dice[2] = temp[6];
+	        dice[5] = temp[1];
+	        dice[6] = temp[5];
+	    }
+
+
 		
 	}
 
