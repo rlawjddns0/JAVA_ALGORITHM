cae8032e769dc9b0e1cc1c1738a06fa7d423e80d
rlawjddns0 <rlawjddns@naver.com>
2021-05-02 00:25:30 +0900

    알고리즘 풀이

---
+++ src/BAEK/month_20210501/BAEK_2502.java
@@ -0,0 +1,63 @@
+package BAEK.month_20210501;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEK_2502 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine()," ");
+		variable[] dp=new variable[31];
+		dp[1]=new variable(1, 0);
+		dp[2]=new variable(0, 1);
+		
+		for(int i=3; i<31; i++) {
+			dp[i]=new variable(dp[i-1].x+dp[i-2].x, dp[i-1].y+dp[i-2].y);
+		}
+//		for(int i=1; i<31; i++) {
+//			System.out.println(dp[i].toString());
+//		}
+		int D=Integer.parseInt(st.nextToken());
+		int K=Integer.parseInt(st.nextToken());
+		
+		int x=1;
+		int y=1;
+		L:while(true) {
+			
+			int X=dp[D].x*x;
+			int Y=dp[D].y*y;
+			if((X+Y)==K)break L;
+			//if((X+Y)>100000)break;
+			y++;
+			if(x+y>100000) {
+				x++;
+				y=x+1;
+			}
+			
+		}
+		
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
+class variable{
+	int x,y;
+
+	public variable(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "variable [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+}
