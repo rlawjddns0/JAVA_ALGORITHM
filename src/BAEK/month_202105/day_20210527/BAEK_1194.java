2ed8430f893ace2e5fcee59d02175e48ac14f44b
rlawjddns0 <rlawjddns0@naver.com>
2021-06-01 18:54:36 +0900

    달이 차오른다.가자 bfs풀이

--- src/BAEK/month_202105/day_20210527/BAEK_1194.java
+++ src/BAEK/month_202105/day_20210527/BAEK_1194.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BAEK_1194 {
 	static int N,M;
 	static char[][] map;
-	static boolean[][] visit;
+	static boolean[][][] visit;
 	static int[] dx= {1,0,-1,0};
 	static int[] dy= {0,1,0,-1};
 	static int starty,startx;
@@ -21,7 +23,7 @@ public class BAEK_1194 {
 		M=Integer.parseInt(st.nextToken());
 		
 		map=new char[N][M];
-		visit=new boolean[N][M];
+		visit=new boolean[N][M][64];
 		
 		for(int i=0; i<N; i++) {
 			char[] tmp=br.readLine().toCharArray();
@@ -33,76 +35,83 @@ public class BAEK_1194 {
 				map[i][j]=tmp[j];
 			}
 		}
-		print();
-		dfs(startx,starty,0);
-		System.out.println(ans);
+		bfs(startx,starty,key);
 		
 		
 	
 	}
-	static int key;
-	static int ans=Integer.MAX_VALUE;
-	private static void dfs(int x, int y,int n) {
-		
-		visit[y][x]=true;
-		if(map[y][x]=='1') {
-			
-			ans=Math.min(ans, n);
-			return;
-		}
-		
-		
-		for(int d=0; d<4; d++) {
-			int nx=x+dx[d];
-			int ny=y+dy[d];
-			
-			if(nx>=M || nx<0 || ny>=N || ny<0)continue;//¸Ê¤·,¤© ¹þ¾î³ª¸é
-			System.out.println("1111111");
-			if(map[ny][nx]=='#') continue; //º®ÀÌ¸é
-			if(visit[ny][nx])continue;
-			System.out.println("2222222");
-			if(map[ny][nx]>='a' && map[ny][nx]<='z') {//key
-				System.out.println("dsfsf");
-				key=key |(1<<map[ny][nx]);
-				visit=new boolean[N][M];
-				visit[ny][nx]=true;
-				dfs(nx,ny,n+1);
-				visit[ny][nx]=false;
+	private static void bfs(int x, int y, int key) {
 
-				
+		Queue<user> q=new LinkedList<>();
+		q.offer(new user(y,x,key,0));
+		visit[y][x][key]=true;
+		while(!q.isEmpty()) {
+			user tmp=q.poll();
+			int cy=tmp.y;
+			int cx=tmp.x;
+			int ckey=tmp.key;
+			int ccnt=tmp.cnt;
+			if(map[cy][cx]=='1') {
+				System.out.println(ccnt);
+				return;
 			}
-			if(map[ny][nx]>='A' && map[ny][nx]<='Z') {
-				System.out.println("33333");
-				if(((0x1 << (map[ny][nx]+32)) & key)==0){
-					System.out.println("SDFSF");
-					visit[ny][nx]=true;
-					dfs(nx,ny,n+1);
-					visit[ny][nx]=false;
+			for(int d=0; d<4; d++) {
+				int nx=cx+dx[d];
+				int ny=cy+dy[d];
+				if(nx>=M || nx<0 || ny>=N || ny<0 || visit[ny][nx][ckey])continue;
+				if(map[ny][nx]=='#')continue;
+				if(map[ny][nx]>='a' && map[ny][nx]<='z') {
+					int tmpkey=ckey |(1<<(map[ny][nx]-'a'));
+					if(!visit[ny][nx][tmpkey]) {
+						visit[ny][nx][tmpkey]=true;
+						visit[cy][cx][ckey]=true;
+						q.offer(new user(ny,nx,tmpkey,ccnt+1));
+					}
+					
+				}
+				else if(map[ny][nx]>='A' && map[ny][nx]<='Z') {
+					int check=(1 << (map[ny][nx] - 'A')) & ckey;
+					
+					if(check>0) {
+						visit[ny][nx][ckey]=true;
+						q.offer(new user(ny,nx,ckey,ccnt+1));
+					}
+				}
+				else {
+					visit[ny][nx][ckey]=true;
+					q.offer(new user(ny,nx,ckey,ccnt+1));
 				}
 			}
-			System.out.println(map[ny][nx]);
-			if(map[ny][nx]=='.' ||map[ny][nx]=='0'||map[ny][nx]=='1') {
-				System.out.println("55555");
-				visit[ny][nx]=true;
-				dfs(nx,ny,n+1);
-				visit[ny][nx]=false;
-			}
-			System.out.println("99999s");
-			
 		}
 		
 		
-		
-		visit[y][x]=false;
-
-		
+		System.out.println("-1");
 		
 		
 	}
+	static int key;
+	
 	static void print() {
 		for(int i=0; i<N; i++) {
 			System.out.println(Arrays.toString(map[i]));
 		}
 	}
 
+}
+class user{
+	int y,x,key,cnt;
+
+	public user(int y, int x, int key,int cnt) {
+		super();
+		this.y = y;
+		this.x = x;
+		this.cnt=cnt;
+		this.key = key;
+	}
+
+	@Override
+	public String toString() {
+		return "user [y=" + y + ", x=" + x + ", key=" + key + "]";
+	}
+	
 }
