ea98ae99b988852756001f5670d64c647a8c0939
rlawjddns0 <rlawjddns0@naver.com>
2021-06-27 21:52:25 +0900

    알고리즘 풀이

---
+++ src/BAEK/month_202106/day_20210626/BAEK_2156.java
@@ -0,0 +1,21 @@
+package BAEK.month_202106.day_20210626;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEK_2156 {
+	static int N;
+	static int 
+	static int[] grape;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		N=Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			grape[i]=Integer.parseInt(br.readLine());
+		}
+				
+	}
+
+}
