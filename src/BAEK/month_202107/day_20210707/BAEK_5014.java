121bc0760b10ababb230a5ba541c009802aedc3a
multicampus <multicampus@DESKTOP-KVCQHCD.mshome.net>
2021-07-07 19:26:10 +0900

    알고리즘 풀이

---
+++ src/BAEK/month_202107/day_20210707/BAEK_5014.java
@@ -0,0 +1,31 @@
+package BAEK.month_202107.day_20210707;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEK_5014 {
+	static int F,S,G,U,D;
+	static boolean[] visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine()," ");
+		
+		F=Integer.parseInt(st.nextToken());
+		S=Integer.parseInt(st.nextToken());
+		G=Integer.parseInt(st.nextToken());
+		U=Integer.parseInt(st.nextToken());
+		D=Integer.parseInt(st.nextToken());
+		
+		visit=new boolean[F+1];
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
